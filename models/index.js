8afaf58c608665d2f5a9d165d259623843c7577d
Rekha Mundada <rekhamundada@gmail.com>
2017-11-10 17:35:27 -0500

    getter methods done

--- models/index.js
+++ models/index.js
@@ -5,29 +5,43 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {
 
 const User = db.define('user', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, allowNull: false
   },
   email: {
-   type: Sequelize.STRING
+   type: Sequelize.STRING,
+    allowNull: false,
+    validate:{
+      isEmail:true
+    }
   }
 });
 
 const Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,allowNull: false
   },
 
   urlTitle: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,allowNull: false
   },
 
   content: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,allowNull: false
   },
 
   status: {
     type: Sequelize.ENUM('open','closed')
+   },
+   date: {
+     type: Sequelize.DATE,
+     defaultValue:Sequelize.NOW
    }
+}, {
+  getterMethods:{
+  route(){
+    return '/wiki/' + this.urlTitle;
+  }
+}
 })
 
 module.exports = {
